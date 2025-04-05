30b6462e3b3b8d758f7b039a260bc2ad5889dc4c
DGh0st <deeppwnage@yahoo.com>
2025-04-05 16:43:05 -0500

    Priority to monster targetting

--- Assets/src/Killable.cs
+++ Assets/src/Killable.cs
@@ -1,24 +1,50 @@
 using UnityEngine;
 using UnityEngine.Events;
 
+public enum Team
+{
+    Neutral,
+    Team1,
+    Team2
+}
+
 public class Killable : MonoBehaviour
 {
-    public UnityEvent OnKilled = new();
+    public UnityEvent<Killable> OnKilled;
     public float MaxHealth = 100.0f;
     public float Health = 100.0f;
+    public float PriorityTargettingRatio = 1.0f;
+    public Team team = Team.Neutral;
 
-    public void Heal(float healAmount)
+    public void Start()
     {
-        Health = Mathf.Min(Health + healAmount, MaxHealth);
+        if (OnKilled == null)
+        {
+            OnKilled = new UnityEvent<Killable>();
+        }
     }
 
-    public void Damage(float damageAmount)
+    public void Heal(Killable healer, float healAmount)
     {
+        if (healer.team == team)
+        {
+            Health = Mathf.Min(Health + healAmount, MaxHealth);
+        }
+    }
+
+    public void Damage(Killable damager, float damageAmount)
+    {
+        if (damager.team == team)
+        {
+            // No friendly fire
+            return;
+        }
+
         Health -= damageAmount;
 
-        if (Health < 0)
+        if (Health <= 0)
         {
-            OnKilled?.Invoke();
+            OnKilled?.Invoke(this);
             Destroy(gameObject);
         }
     }
--- Assets/src/Monster.cs
+++ Assets/src/Monster.cs
@@ -1,67 +1,153 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UIElements;
+using static UnityEngine.GraphicsBuffer;
 
 public class Monster : MonoBehaviour
 {
     private const string PlayerTag = "Player";
     private const string TargetableTag = "Targetable";
 
-    private List<GameObject> Targetables = new();
-    public float Speed = 1f;
+    public float MovementSpeed = 2.5f;
     public float RotationSpeed = 10000f;
     public float Damage = 20.0f;
+    public float DamageCooldown = 1;
+    public float AttackRange = 1;
+
+    private List<Killable> Targetables = new();
+    private float lastAttackTime = float.MinValue;
 
     void Start()
     {
-        UpdateTargetables();
+        var killables = GameObject.FindObjectsByType<Killable>(FindObjectsSortMode.None);
+        foreach (var killable in killables)
+        {
+            if (killable.team != Team.Neutral && killable.team != GetComponent<Killable>().team)
+            {
+                AddTarget(killable);
+            }
+        }
+    }
+
+    void OnDestroy()
+    {
+        if (Targetables.Count > 0)
+        {
+            Targetables.ForEach(x => x.OnKilled.RemoveListener(RemoveTarget));
+        }
     }
 
     void Update()
     {
-        var target = SelectTarget();
+        var targetableGameObjects = SelectTarget();
+        if (targetableGameObjects == null)
+        {
+            return;
+        }
 
-        float angle = Mathf.Atan2(target.transform.position.y - transform.position.y, target.transform.position.x - transform.position.x) * Mathf.Rad2Deg;
-
-        transform.SetPositionAndRotation(
-            Vector3.MoveTowards(transform.position, target.transform.position, Time.deltaTime * Speed),
-            Quaternion.RotateTowards(transform.rotation, Quaternion.AngleAxis(angle, Vector3.forward), Time.deltaTime * RotationSpeed));
+        if (CanAttackTarget(targetableGameObjects))
+        {
+            AttackTarget(targetableGameObjects);
+        }
+        else
+        {
+            MoveTowardsTarget(targetableGameObjects);
+        }
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag(PlayerTag) ||  collision.gameObject.tag == TargetableTag)
+        if (collision.gameObject.ConvertTo<Killable>())
         {
-            collision.gameObject.ConvertTo<Killable>().Damage(Damage);
+            GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
         }
     }
 
-    private GameObject SelectTarget()
+    void OnCollisionExit2D(Collision2D collision)
     {
-        var targetables = Targetables.Select(target => new TargetableGameObjects
+        if (collision.gameObject.ConvertTo<Killable>())
+        {
+            GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
+        }
+    }
+
+    protected bool CanAttackTarget(KillableTarget killableTarget)
+    {
+        return killableTarget.Distance < AttackRange;
+    }
+
+    protected void AttackTarget(KillableTarget killableTarget)
+    {
+        DamageTarget(killableTarget.Target);
+    }
+
+    protected void MoveTowardsTarget(KillableTarget killableTarget)
+    {
+        var target = killableTarget.Target;
+        float angle = Mathf.Atan2(target.transform.position.y - transform.position.y, target.transform.position.x - transform.position.x) * Mathf.Rad2Deg;
+        GetComponent<Rigidbody2D>().MovePositionAndRotation(
+            Vector3.MoveTowards(transform.position, target.transform.position, Time.deltaTime * MovementSpeed),
+            Quaternion.RotateTowards(transform.rotation, Quaternion.AngleAxis(angle, Vector3.forward), Time.deltaTime * RotationSpeed));
+    }
+
+    private KillableTarget SelectTarget()
+    {
+        if (Targetables.Count == 0)
+        {
+            return null;
+        }
+
+        var targetables = Targetables.Select(target => new KillableTarget
         {
             Target = target,
             Distance = Vector3.Distance(target.transform.position, transform.position)
         });
 
-        return targetables.Aggregate((currentMin, targetable) => targetable.Distance < currentMin.Distance ? targetable : currentMin).Target;
+        return targetables.Aggregate((currentMin, targetable) => SelectPriorityTarget(currentMin, targetable));
     }
 
-    private void UpdateTargetables()
+    private KillableTarget SelectPriorityTarget(KillableTarget currentTarget, KillableTarget newPotentialTarget)
     {
-        Targetables.ForEach(x => x.ConvertTo<Killable>().OnKilled.RemoveListener(UpdateTargetables));
+        if (currentTarget == null)
+        {
+            return newPotentialTarget;
+        }
 
-        Targetables.Add(GameObject.FindGameObjectWithTag(PlayerTag));
-        Targetables.AddRange(GameObject.FindGameObjectsWithTag(TargetableTag));
-
-        Targetables.ForEach(x => x.ConvertTo<Killable>().OnKilled.AddListener(UpdateTargetables));
+        return newPotentialTarget.PriorityDistance < currentTarget.PriorityDistance ? newPotentialTarget : currentTarget;
     }
 
-    private class TargetableGameObjects
+    public void RemoveTarget(Killable killable)
     {
-        public GameObject Target;
+        Targetables.Remove(killable);
+    }
+
+    public void AddTarget(Killable target)
+    {
+        Targetables.Add(target);
+        target.OnKilled.AddListener(RemoveTarget);
+    }
+
+    private void DamageTarget(Killable target)
+    {
+        if (lastAttackTime + DamageCooldown > Time.time)
+        {
+            return;
+        }
+
+        target.Damage(GetComponent<Killable>(), Damage);
+        lastAttackTime = Time.time;
+    }
+
+    protected class KillableTarget
+    {
+        public Killable Target;
         public float Distance;
-        public bool Killable;
+        public float PriorityTargettingRatio;
+
+        public float PriorityDistance => Distance / Target.PriorityTargettingRatio;
     }
 }
