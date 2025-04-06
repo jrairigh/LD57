ba0ff8610555084e192e255161403f00119d1541
Ian <lukeip14cyan@gmail.com>
2025-04-06 15:22:37 -0500

    camera

--- Assets/src/ReticleController.cs
+++ Assets/src/ReticleController.cs
@@ -1,10 +1,18 @@
+using TMPro;
 using UnityEngine;
 
 public class ReticleController : MonoBehaviour
 {
+    private Camera m_camera;
+
+    private void Awake()
+    {
+        m_camera = Camera.main;
+    }
+
     void Update()
     {
-        var mouse_world_pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        var mouse_world_pos = m_camera.ScreenToWorldPoint(Input.mousePosition);
         mouse_world_pos.z = 0;
         transform.position = mouse_world_pos;
     }
